459185f7604430d6a3e578b5febfe1dea958f146
Miguel Vale Teixeira <miguel@kediumm.com>
2017-03-16 22:14:57 +0000

    increase IndicatorsEM enum

---
+++ src/main/java/com/kediumm/kednasdake/controller/prefs/IndicatorsEM.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2016 Miguel Vale Teixeira
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author: Miguel Vale Teixeira
+ * @data: 2016/01/16
+ * 
+ * This source code is intended for educational and illustrative purposes only
+ * and is provided AS-IS without warranty of any kind.
+ * 
+ */
+package com.kediumm.kednasdake.controller.prefs;
+
+public enum IndicatorsEM {
+	
+	BUY("BUY"),
+	
+	SELL("SELL")
+    
+    ;
+	
+	private String var_Value;
+	
+	private IndicatorsEM(String par_Value) {
+		
+		var_Value = par_Value;
+	}
+
+	public String getValue() {
+		
+		return var_Value;
+	}	
+
+}
